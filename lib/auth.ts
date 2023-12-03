07593ae69f262c7a6e7139884fec2853f3f6cae8
Stephan Niendorf <stephan@stephanmitph.com>
2023-12-03 10:24:23 +0100

    change route imports

---
+++ lib/auth.ts
@@ -0,0 +1,24 @@
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { AuthOptions } from 'next-auth';
+
+import prisma from '@/lib/prismadb';
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+	],
+	pages: {
+		signIn: '/sign-in',
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+};
